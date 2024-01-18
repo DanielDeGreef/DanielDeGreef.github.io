87573c50d027fd33b9fd54d445826fc479f212f6
DanielDeGreef <ddegreef2@gmail.com>
2024-01-18 16:10:01 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,38 @@
-const myImage = document.querySelector("img");
-myImage.setAttribute("src", "https://mdn.github.io/beginner-html-site-styled/images/firefox-icon.png");
+// Image switcher code
 
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "https://mdn.github.io/beginner-html-site-styled/images/firefox-icon.png") {
-    myImage.setAttribute("src", "https://avatars.dzeninfra.ru/get-zen_doc/40274/pub_5d3ca3941e8e3f00b21ea245_5d3caede97b5d400af6850a1/scale_1200");
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','images/firefox2.png');
   } else {
-    myImage.setAttribute("src", "https://mdn.github.io/beginner-html-site-styled/images/firefox-icon.png");
+    myImage.setAttribute ('src','images/firefox-icon.png');
   }
-};
+}
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  const myName = prompt("Please enter your name.");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = `Mozilla is cool, ${myName}`;
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
 }
 
-if (!localStorage.getItem("name")) {
+if(!localStorage.getItem('name')) {
   setUserName();
 } else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
-myButton.onclick = () => {
+myButton.onclick = function() {
   setUserName();
-};
+}
